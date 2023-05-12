07f2be8ea5e18d1b33fdca109677320f1e38bc76
Michele Rosa <michele.jeniffer.mr@gmail.com>
2023-05-12 12:42:39 -0300

    GameMinDTO, GameRepository, GameService, GameController

---
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dslist.services;
+
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.repositories.GameRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    //INJETANDO Repositório
+    @Autowired
+    private GameRepository gameRepository;
+
+    //MÉTODO - do tipo lista
+    public List<GameMinDTO> findAll(){
+        List<Game> result = gameRepository.findAll();
+        //map - para tranformar algo em outro coisa
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+    }
+}
